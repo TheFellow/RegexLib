99ed3e2fdf88aecae1857c0a9efc1028fed6f47f
Ryan Harris <ThomasRyanHarris@gmail.com>
2015-08-29 10:49:46 -0700

    Create the basic Context class

---
+++ RegexLib/RegexLib/Context.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexLib
+{
+    class Context
+    {
+        /// <summary>
+        /// The string we're attempting to match
+        /// </summary>
+        public readonly string matchString;
+
+        /// <summary>
+        /// The current offset in the string
+        /// </summary>
+        public int offset;
+
+        public Context(string matchString)
+        {
+            this.matchString = matchString;
+        }
+
+        #region Match string helper properties
+
+        public int Length => matchString.Length;
+        public char curr => (offset >= 0 && offset < matchString.Length) ? matchString[offset] : '\0';
+
+        #endregion
+    }
+}
