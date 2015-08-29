2e6335723a17ee281182a3c415b63bed8e11a698
Ryan Harris <ThomasRyanHarris@gmail.com>
2015-08-29 10:58:46 -0700

    Add Empty

---
+++ RegexLib/RegexLib/Core/Empty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexLib.Core
+{
+    /// <summary>
+    /// The Empty token is a zero-width token which will always match, but has not alternatives
+    /// </summary>
+    class Empty : IMatch
+    {
+        public bool Match(Context context) => true;
+        public bool MatchNext(Context context) => false;
+    }
+}
--- RegexLib/RegexLib/IMatch.cs
+++ RegexLib/RegexLib/IMatch.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RegexLib
+﻿namespace RegexLib
 {
     /// <summary>
     /// The interface for a regular expression token
