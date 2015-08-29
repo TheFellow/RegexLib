f217ce99f1634fc491514c284ff11bdd8f6f10ae
Ryan Harris <ThomasRyanHarris@gmail.com>
2015-08-29 10:56:55 -0700

    Added IMatch

---
+++ RegexLib/RegexLib/IMatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexLib
+{
+    /// <summary>
+    /// The interface for a regular expression token
+    /// </summary>
+    interface IMatch
+    {
+        /// <summary>
+        /// This method is the first called in the backtracking algorithm.
+        /// </summary>
+        bool Match(Context context);
+
+        /// <summary>
+        /// This method is called during backtracking, and should never be called unless a successful Match has already happened.
+        /// </summary>
+        bool MatchNext(Context context);
+    }
+}
